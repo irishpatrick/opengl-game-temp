7ac1d3a60481bc56b47480d1f5d0f00196149764
Patrick Roche <rochep63@gmail.com>
2018-09-04 20:24:28 -0400

    more work on shader class

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -1,4 +1,8 @@
 #include "Shader.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdint>
 
 Shader::Shader()
 {
@@ -8,4 +12,27 @@ Shader::Shader()
 Shader::~Shader()
 {
 
+}
+
+void Shader::loadShader(const std::string& fn, GLenum type)
+{
+	std::string content = readFile(fn);
+	char* data = const_cast<char*>(content.c_str());
+	m_vsid = glCreateShader(type);
+	glShaderSource(m_vsid, 1, &data, &(int32_t)content.length());
+}
+
+std::string Shader::readFile(const std::string& fn)
+{
+	std::ifstream in(fn);
+	if (in.fail())
+	{
+		std::cout << fn << " does not exist" << std::endl;
+		return "";
+	}
+
+	std::stringstream buffer;
+	buffer << in.rdbuf();
+	std::string content = buffer.str();
+	return content;
 }
--- src/Shader.hpp
+++ src/Shader.hpp
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <GL/glew.h>
+#include <string>
+
 class Shader
 {
 public:
 	Shader();
 	~Shader();
+
+	void loadShader(const std::string&, GLenum);
+
+	GLuint getProgramId()
+	{
+		return m_pid;
+	}
+
+private:
+	GLuint m_vsid;
+	GLuint m_gsid;
+	GLuint m_fsid;
+	GLuint m_pid;
+	std::string readFile(const std::string&);
 };
 
