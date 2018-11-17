2491e3ae2741736a1089153a641f9275d163e17a
Patrick Roche <rochep63@gmail.com>
2018-11-17 16:58:53 -0500

    adding more files

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,1 +1,42 @@
 #include "Mesh.hpp"
+
+void Mesh::init()
+{
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        vertices.size() * sizeof(Vertex),
+        &vertices[0],
+        GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(
+        0, 3,
+        GL_FLOAT,
+        GL_FALSE,
+        sizeof(Vertex),
+        (void*)0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3,
+        GL_FLOAT,
+        GL_FALSE,
+        sizeof(Vertex),
+        (void*)offsetof(Vertex, normal));
+
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(
+        2, 2, 
+        GL_FLOAT,
+        GL_FALSE,
+        sizeof(Vertex),
+        (void*)offsetof(Vertex, texture));
+
+    glBindVertexArray(0);
+}
--- src/Mesh.hpp
+++ src/Mesh.hpp
@@ -1,11 +1,24 @@
 #pragma once
 
+#include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/ext.hpp>
+#include <vector>
+#include <gl/glew.h>
+
 #include "Model.hpp"
 
-class Mesh : public Model
+typedef struct _Vertex
+{
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texture;
+} Vertex;
+
+class Mesh
 {
 public:
-    Mesh() : Model()
+    Mesh()
     {
 
     }
@@ -14,4 +27,11 @@ public:
     {
 
     }
+
+    void init();
+    void draw();
+
+private:
+    std::vector<Vertex> vertices;
+    GLuint vao, vbo, ebo;
 };
